8c849ee643b32e2172f59be02f90bb3b8a4f2c8a
jauseg <jauseg@gmail.com>
2013-11-11 14:01:19 +0400

    first view is working

--- src/ru/jauseg/spacelife/ObjectMCX.java
+++ src/ru/jauseg/spacelife/ObjectMCX.java
@@ -8,30 +8,36 @@ import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
 import js.engine.BufferAllocator;
 
-public class ObjectMCX {
+public class ObjectMCX
+{
 	private FloatBuffer points = null;
 	private FloatBuffer normals = null;
 	private FloatBuffer textures = null;
-	private ByteBuffer indexes = null;
+	private ShortBuffer indexes = null;
 
-	public ObjectMCX(InputStream stream) {
-		try {
+	public ObjectMCX(InputStream stream)
+	{
+		try
+		{
 			byte[] data = new byte[stream.available()];
 			stream.read(data);
 			ByteBuffer bb = ByteBuffer.wrap(data);
 			bb.order(ByteOrder.LITTLE_ENDIAN);
 			initFrom(bb);
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	private void initFrom(ByteBuffer buffer) {
+	private void initFrom(ByteBuffer buffer)
+	{
 		int header_id = buffer.getInt();
 
 		int vnum = buffer.getInt();
@@ -42,11 +48,13 @@ public class ObjectMCX {
 		points = BufferAllocator.createFloatBuffer(vnum * 3);
 		normals = BufferAllocator.createFloatBuffer(vnum * 3);
 
-		if (texture != 0) {
+		if (texture != 0)
+		{
 			textures = BufferAllocator.createFloatBuffer(vnum * 2);
 		}
 
-		for (int i = 0; i < vnum; i++) {
+		for (int i = 0; i < vnum; i++)
+		{
 			points.put(buffer.getFloat());
 			points.put(buffer.getFloat());
 			points.put(buffer.getFloat());
@@ -54,41 +62,46 @@ public class ObjectMCX {
 			normals.put(buffer.getFloat());
 			normals.put(buffer.getFloat());
 
-			if (texture != 0) {
+			if (texture != 0)
+			{
 				textures.put(buffer.getFloat());
 				textures.put(buffer.getFloat());
 			}
 		}
+		
+		indexes = BufferAllocator.createBuffer(fnum*2*3).asShortBuffer();
 
-		indexes = BufferAllocator.createBuffer(fnum * 2 * 3);
-
-		for (int i = 0; i < fnum; i++) {
-			indexes.putShort(buffer.getShort());
-			indexes.putShort(buffer.getShort());
-			indexes.putShort(buffer.getShort());
+		for (int i = 0; i < fnum; i++)
+		{
+			indexes.put(buffer.getShort());
+			indexes.put(buffer.getShort());
+			indexes.put(buffer.getShort());
 		}
 
 		points.position(0);
 		normals.position(0);
-		if (textures != null) {
+		if (textures != null)
+		{
 			textures.position(0);
 		}
 		indexes.position(0);
 	}
 
-	public void setupBuffers(GL10 gl) {
+	public void setupBuffers(GL10 gl)
+	{
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, points);
 		gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
 		gl.glNormalPointer(GL10.GL_FLOAT, 0, normals);
-		if (textures != null) {
+		if (textures != null)
+		{
 			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
 			gl.glTexCoordPointer(2, GL10.GL_TEXTURE_COORD_ARRAY, 0, textures);
 		}
 	}
 
-	public void draw(GL10 gl) {
-		gl.glDrawElements(GL10.GL_TRIANGLES, indexes.capacity() >> 1,
-				GL10.GL_UNSIGNED_SHORT, indexes);
+	public void draw(GL10 gl)
+	{
+		gl.glDrawElements(GL10.GL_TRIANGLES, indexes.capacity() , GL10.GL_UNSIGNED_SHORT, indexes);
 	}
 }
--- src/ru/jauseg/spacelife/WallpaperRenderer.java
+++ src/ru/jauseg/spacelife/WallpaperRenderer.java
@@ -5,6 +5,7 @@ import java.nio.Buffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
 
 import js.engine.FrameRateCalculator;
 import js.engine.FrameRateCalculator.FrameRateUpdateInterface;
@@ -41,24 +42,41 @@ public class WallpaperRenderer implements GLWallpaperService.Renderer, FrameRate
 	{
 		Log.v(TAG, "onSurfaceCreated");
 
-		gl.glDisable(GL10.GL_DEPTH_TEST);
-		gl.glDisable(GL10.GL_LIGHTING);
-		gl.glDisable(GL10.GL_CULL_FACE);
+		float[] cl0 = new float[] { 0.75f, 0.75f, 0.75f, 1.0f };
+		float[] fog = new float[] { 0.0f, 0.5f, 0.75f, 1.0f };
 
-		gl.glDisable(GL10.GL_DITHER);
-
-		gl.glEnable(GL10.GL_BLEND);
-		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE);
-
-		gl.glEnable(GL10.GL_TEXTURE_2D);
-
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		gl.glClearDepthf(1.0f);
+		gl.glDepthFunc(GL10.GL_LESS);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
 		gl.glShadeModel(GL10.GL_SMOOTH);
-		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
 
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+		gl.glTexEnvf(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE, GL10.GL_REPLACE);
 
-		gl.glClearColor(0, 0, 0, 0);
+		gl.glEnable(GL10.GL_CULL_FACE);
+		gl.glCullFace(GL10.GL_BACK);
+
+		gl.glEnable(GL10.GL_LIGHTING);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glEnable(GL10.GL_COLOR_MATERIAL);
+		// gl.glColorMaterial(GL10.GL_FRONT, GL10.GL_SPECULAR);
+		gl.glEnable(GL10.GL_COLOR_MATERIAL);
+
+		/*
+		 * glEnable(GL_FOG); glFogi(GL_FOG_MODE,GL_EXP);
+		 * glFogfv(GL_FOG_COLOR,fog); glFogf(GL_FOG_DENSITY,0.02);
+		 */
+
+		// somelight.InitLight();
+		// somelight.SetDiffuseColor(1.0f, 1.0f, 0.0f);
+		// somelight.SetSpecularColor(1.0f, 1.0f, 0.0f);
+		// somelight.SetProfile(1.0f, 0.05f, 0.0f);
+
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 	}
 
 	@Override
@@ -66,18 +84,12 @@ public class WallpaperRenderer implements GLWallpaperService.Renderer, FrameRate
 	{
 		Log.v(TAG, "onSurfaceChanged");
 
+		gl.glViewport(0, 0, width, height);
+
 		gl.glMatrixMode(GL10.GL_PROJECTION);
 		gl.glLoadIdentity();
-		
-		gl.glDepthRangef(-100, 100);
-		//gl.glDisable(gl.gl_cu)
-
-		gl.glViewport(0, 0, width, height);
-		GLU.gluOrtho2D(gl, 0, width, height, 0);
-
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-
+		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 2.0f, 2000.0f);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
 	}
 
 	@Override
@@ -87,12 +99,13 @@ public class WallpaperRenderer implements GLWallpaperService.Renderer, FrameRate
 
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-		gl.glLoadIdentity();
-		//gl.glTranslatef(0, 0, -50);
-
+		gl.glPushMatrix();
+		gl.glTranslatef(0, 0, -100);
+		gl.glRotatef(45, 1, 0, 0);
 		gl.glColor4f(1, 1, 1, 0);
 		ship.setupBuffers(gl);
-		ship.draw(gl);
+for(int i=0;i<64;i++)		ship.draw(gl);
+		gl.glPopMatrix();
 
 		fps.frameDone();
 	}
